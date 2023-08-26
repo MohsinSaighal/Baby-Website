89dbd461b85f60ea117d42b415eb787c90ff5314
MS <mohsint908@gmail.com>
2023-08-27 02:10:42 +0500

    added commit

--- src/Page/PreSale.js
+++ src/Page/PreSale.js
@@ -18,6 +18,9 @@ import { useWeb3Modal } from "@web3modal/react";
 import ProgressBar from "./Progress";
 import USDT from "../assets/Phone/usdt.svg";
 import { useAccount, useDisconnect } from "wagmi";
+import PreSaleAbi from "../components/Contracts/PreSale.json";
+import presaleAddress from "../components/Contracts/PreSaleAddress.json";
+import { ethers } from "ethers";
 
 const theme = createTheme({
   palette: {
@@ -103,9 +106,11 @@ const PreSale = ({ targetDate }) => {
   const { open, close } = useWeb3Modal();
   const { isConnected } = useAccount();
   const { disconnect } = useDisconnect();
-
   const classes = useStyles();
   const [isShown, setIsShown] = useState(false);
+  const [amountRaised, setAmountRaised] = useState();
+  const [input,setInput]= useState(0)
+  const [tokenPrice,setTokenPrice]=useState(0)
 
   const [timeLeft, setTimeLeft] = useState({
     days: 5,
@@ -121,7 +126,53 @@ const PreSale = ({ targetDate }) => {
     setIsShown(isOpen);
   };
 
+  const getValue = (e) => {
+    setInput(e.target.value)
+  };
+
+  const getTokenPrice = async () => {
+    try {
+      const providers = new ethers.getDefaultProvider(
+        "https://data-seed-prebsc-1-s3.binance.org:8545/"
+      );
+
+      const contract = new ethers.Contract(
+        presaleAddress.address,
+        PreSaleAbi,
+        providers
+      );
+      const decimals = 1000000000000000000
+      const presalePrice = await contract.getRate();
+      console.log(presalePrice.toString())
+      setTokenPrice(presalePrice.toString()/decimals);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+
+  const getAmountRaised = async () => {
+    try {
+      const providers = new ethers.getDefaultProvider(
+        "https://data-seed-prebsc-1-s3.binance.org:8545/"
+      );
+
+      const contract = new ethers.Contract(
+        presaleAddress.address,
+        PreSaleAbi,
+        providers
+      );
+      const presalePrice = await contract.weiRaised();
+      setAmountRaised(presalePrice.toString());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
+    getAmountRaised();
+    getTokenPrice()
     const interval = setInterval(() => {
       const now = new Date().getTime();
       const difference = targetDate - now;
@@ -147,171 +198,201 @@ const PreSale = ({ targetDate }) => {
 
   return (
     <Stack
-    id="Buy"
-    spacing={10}
+      id="Buy"
+      spacing={10}
       sx={{
-        height: { xs:"100vh",md: "350vh", lg: "90vh" },
+        height: { xs: "100vh", md: "350vh", lg: "90vh" },
         background: "#000235",
         justifyContent: "center",
         alignItems: "center",
-        width:{xs:"96%"}
+        width: { xs: "96%" },
       }}
     >
       <Stack>
-      <Typography sx={{color:"#F39036",fontFamily:"Urbanist",fontSize:{xs:"30px",lg:"50px"},fontWeight:"800"}}>Buy BabyFarm  Here</Typography>
-      </Stack>
-      <Stack width={{xs:"90%",lg:"30%"}}>
-      <Card
-        sx={{
-          borderRadius: "1.3rem",
-          background: `linear-gradient(90deg, black, black, black)`,
-          border: "1px solid",
-          borderImage:
-            "linear-gradient(90deg, rgba(249, 195, 6, 0.00), rgba(249, 195, 6, 0.30),rgba(249, 195, 6, 0.00)) 1",
-          marginBottom: "10px", // Add bottom margin to create space
-          width:{xs:"111%"}
-        }}
-      >
-        <CardContent
+        <Typography
           sx={{
-            flex: "1 0 auto",
-            backgroundColor: "black",
-            color: "white",
+            color: "#F39036",
+            fontFamily: "Urbanist",
+            fontSize: { xs: "30px", lg: "50px" },
+            fontWeight: "800",
           }}
         >
-          <div className={classes.countdownContainer}>
-            <div className={classes.countdownItem}>
-              <div className={classes.countdownValue}>{timeLeft.days}</div>
-              <div className={classes.countdownLabel}>Days</div>
-            </div>
-            <div className={classes.countdownItem}>
-              <div className={classes.countdownValue}>{timeLeft.hours}</div>
-              <div className={classes.countdownLabel}>Hours</div>
-            </div>
-            <div className={classes.countdownItem}>
-              <div className={classes.countdownValue}>{timeLeft.minutes}</div>
-              <div className={classes.countdownLabel}>Minutes</div>
-            </div>
-            <div className={classes.countdownItem}>
-              <div className={classes.countdownValue}>{timeLeft.seconds}</div>
-              <div className={classes.countdownLabel}>Seconds</div>
-            </div>
-          </div>
-          <Grid
-            container
-            justifyContent="center"
-            direction="column"
-            sx={{ borderRadius: "0px" }}
-          >
-            <Grid item>
-              <ProgressBar />
-            </Grid>
-            <br />
-            <Grid sx={{ textAlign: "center" }} spacing={3}>
-              <Typography sx={{ fontFamily: "Urbanist",fontSize:{xs:"15px"} }}>
-                USDT raised: $0 / $6,700,000
-              </Typography>
-            </Grid>
-          </Grid>
-        </CardContent>
+          Buy BabyFarm Here
+        </Typography>
+      </Stack>
+      <Stack width={{ xs: "90%", lg: "30%" }}>
         <Card
           sx={{
-            backgroundColor: "black",
-            color: "#fefefe",
-            borderRadius: "0px",
+            borderRadius: "1.3rem",
+            background: `linear-gradient(90deg, black, black, black)`,
+            border: "1px solid",
+            borderImage:
+              "linear-gradient(90deg, rgba(249, 195, 6, 0.00), rgba(249, 195, 6, 0.30),rgba(249, 195, 6, 0.00)) 1",
+            marginBottom: "10px", // Add bottom margin to create space
+            width: { xs: "111%" },
           }}
         >
-          <CardContent>
-            <Divider variant="middle">1 Babicu = $1</Divider>
-            <Divider variant="middle">Listing Price: $1</Divider>
-            <Stack
-              sx={{
-                backgroundColor: "#444654",
-                borderRadius: "5px",
-                height: "30px",
-                marginTop: "3px",
-                padding: "4px",
-                textAlign: "center",
-                "@media (max-width: 500px)": {
-                  fontSize: "12px", // Adjust the font size for smaller screens
-                },
-              }}
+          <CardContent
+            sx={{
+              flex: "1 0 auto",
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
+            <div className={classes.countdownContainer}>
+              <div className={classes.countdownItem}>
+                <div className={classes.countdownValue}>{timeLeft.days}</div>
+                <div className={classes.countdownLabel}>Days</div>
+              </div>
+              <div className={classes.countdownItem}>
+                <div className={classes.countdownValue}>{timeLeft.hours}</div>
+                <div className={classes.countdownLabel}>Hours</div>
+              </div>
+              <div className={classes.countdownItem}>
+                <div className={classes.countdownValue}>{timeLeft.minutes}</div>
+                <div className={classes.countdownLabel}>Minutes</div>
+              </div>
+              <div className={classes.countdownItem}>
+                <div className={classes.countdownValue}>{timeLeft.seconds}</div>
+                <div className={classes.countdownLabel}>Seconds</div>
+              </div>
+            </div>
+            <Grid
+              container
+              justifyContent="center"
+              direction="column"
+              sx={{ borderRadius: "0px" }}
             >
-              <Typography
-                fontSize={{ xs: "0.8rem", sm: "1rem" }}
-                sx={{ fontFamily: "Urbanist" }}
-              >
-                Buy Our Baby Farm Token Now
-              </Typography>
-            </Stack>
-
-            <Stack
-              direction="row"
-              spacing={2}
-              sx={{ justifyContent: "center", paddingTop: "10px" }}
-            >
-              <Typography
+              <Grid item>
+                <ProgressBar />
+              </Grid>
+              <br />
+              <Grid sx={{ textAlign: "center" }} spacing={3}>
+                <Typography
+                  sx={{ fontFamily: "Urbanist", fontSize: { xs: "15px" } }}
+                >
+                  USDT raised: ${amountRaised / 1000000000000000000} /
+                  $6,700,000
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+          <Card
+            sx={{
+              backgroundColor: "black",
+              color: "#fefefe",
+              borderRadius: "0px",
+            }}
+          >
+            <CardContent>
+              <Divider variant="middle">1 Babicu = ${tokenPrice}</Divider>
+              <Divider variant="middle">Listing Price: ${tokenPrice}</Divider>
+              <Stack
                 sx={{
-                  color: "white",
+                  backgroundColor: "#444654",
+                  borderRadius: "5px",
+                  height: "30px",
+                  marginTop: "3px",
+                  padding: "4px",
                   textAlign: "center",
-                  display: "flex",
-                  alignItems: "center",
+                  "@media (max-width: 500px)": {
+                    fontSize: "12px", // Adjust the font size for smaller screens
+                  },
                 }}
               >
-                Buy Baby Farm in
-              </Typography>
-              <Button
-                sx={{ borderColor: "gray", color: "white" }}
-                variant="outlined"
-                startIcon={<Avatar src={USDT} alt="USDT Logo" />}
+                <Typography
+                  fontSize={{ xs: "0.8rem", sm: "1rem" }}
+                  sx={{ fontFamily: "Urbanist" }}
+                >
+                  Buy Our Baby Farm Token Now
+                </Typography>
+              </Stack>
+
+              <Stack
+                direction="row"
+                spacing={2}
+                sx={{ justifyContent: "center", paddingTop: "10px" }}
               >
-                USDT
-              </Button>
-            </Stack>
-            <br />
-            <Stack direction="column" spacing={2}>
-              <WhiteTextField label="Amount in USDT you Pay" />
-              <WhiteTextField label="Amount in BabyFarm you receive " />
-              {isConnected ? (
-                <Button
-                  variant="outlined"
+                <Typography
                   sx={{
                     color: "white",
-                    textTransform: "uppercase",
-                    borderColor: "white",
-                    "&:hover": {
-                      backgroundColor: "#1D2226",
-                      borderColor: "white",
-                      color: "white",
-                    },
+                    textAlign: "center",
+                    display: "flex",
+                    alignItems: "center",
                   }}
-                  className={classes.customButton}
-                  onClick={() => disconnect()}
                 >
-                  Buy Now!
-                </Button>
-              ) : (
+                  Buy Baby Farm in
+                </Typography>
                 <Button
+                  sx={{ borderColor: "gray", color: "white" }}
                   variant="outlined"
-                  sx={{
-                    color: "white",
-                    borderColor: "white",
-                    "&:hover": {
-                      backgroundColor: "#1D2226",
-                      borderColor: "white",
-                      color: "white",
-                    },
-                  }}
-                  className={classes.customButton}
-                  onClick={() => open()}
+                  startIcon={<Avatar src={USDT} alt="USDT Logo" />}
                 >
-                  Connect Wallet
+                  USDT
                 </Button>
-              )}
-            </Stack>
-          </CardContent>
+              </Stack>
+              <br />
+              <Stack direction="column" spacing={2}>
+                <ThemeProvider theme={theme}>
+                  <TextField
+                    label={"Amount in USDT you Pay"}
+                    fullWidth
+                    variant="outlined"
+                    onChange={(e) => getValue(e)}
+                  />
+                </ThemeProvider>
+                <ThemeProvider theme={theme}>
+                  <Typography>Amount in Baby Farm You Receive</Typography>
+                  <TextField
+                     placeholder={tokenPrice * input}
+                     value={tokenPrice * input}
+                     fullWidth
+                     variant="outlined"
+                     InputProps={{
+                       readOnly: true, // Make the input read-only
+                     }}
+                  />
+                </ThemeProvider>
+                {isConnected ? (
+                  <Button
+                    variant="outlined"
+                    sx={{
+                      color: "white",
+                      textTransform: "uppercase",
+                      backgroundColor:"#1D2226",
+                      borderColor: "white",
+                      "&:hover": {
+                        backgroundColor: "#1D2226",
+                        borderColor: "white",
+                        color: "white",
+                      },
+                    }}
+                    className={classes.customButton}
+                    onClick={() => disconnect()}
+                  >
+                    Buy Now!
+                  </Button>
+                ) : (
+                  <Button
+                    variant="outlined"
+                    sx={{
+                      color: "white",
+                      borderColor: "white",
+                      "&:hover": {
+                        borderColor: "white",
+                        color: "white",
+                      },
+                    }}
+                    className={classes.customButton}
+                    onClick={() => open()}
+                  >
+                    Connect Wallet
+                  </Button>
+                )}
+              </Stack>
+            </CardContent>
+          </Card>
         </Card>
-      </Card>
       </Stack>
     </Stack>
   );
