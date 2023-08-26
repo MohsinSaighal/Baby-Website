cdb7e04df00fc30d2aa22465e2424bfea6d906e8
MS <mohsint908@gmail.com>
2023-08-26 21:22:03 +0500

    added commit

--- src/Page/Tokenomics.js
+++ src/Page/Tokenomics.js
@@ -19,7 +19,7 @@ function Tokenomic() {
       spacing={20}
       sx={{
         maxWidth: "100%",
-        height: { xs: "200vh",sm:"200vh", lg: "180vh" },
+        height: { xs: "200vh",sm:"220vh", lg: "180vh" },
         background: "#000235",
         justifyContent: "center",
         alignItems: "center",
